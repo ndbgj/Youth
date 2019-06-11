feb058461cdfd7c170e6f0f44a0311def110d737
ndbgj <2224676448@qq.com>
2019-06-11 15:48:05 +0800

    删除默认配置文件配置源引用yml配置

--- src/main/java/com/youth/Application.java
+++ src/main/java/com/youth/Application.java
@@ -1,5 +1,6 @@
 package com.youth;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
+@MapperScan("com.youth.domain")
 public class Application {
 
 	public static void main(String[] args) {
--- src/main/java/com/youth/controller/ControllerTest.java
+++ src/main/java/com/youth/controller/ControllerTest.java
@@ -1,5 +1,8 @@
 package com.youth.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -7,11 +10,14 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ControllerTest {		
-		
+	
+	@Value("${com.li.wu}")
+	public String cao;
+	
 	@RequestMapping("/mmp")
     public ModelAndView index(){
 		ModelAndView ok=new ModelAndView("/index");
-		ok.addObject("welcome","nmm");
+		ok.addObject("welcome",cao);
         return ok;
     }
 }
